6916fc08f48c1f6580616783372661764a3386ad
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-17 19:04:04 -0600

    feat(frontend): add view to show news list

---
+++ app/Views/news/index.php
@@ -0,0 +1,13 @@
+<h2><?= esc( $title ) ?></h2>
+<?php if( $news_list !== [] ) : ?>
+    <?php foreach( $news_list as $new_item ) : ?>
+        <h3><?= esc( $new_item['title'] ) ?></h3>
+        <div class="main">
+            <p><?= esc( $new_item['body'] ) ?></p>
+        </div>
+        <p><a href="/news/<?= esc($new_item['slug'], 'url') ?>">View article</a></p>
+    <?php endforeach; ?>
+<?php else : ?>
+    <h3>No news</h3>
+    <p>Uneable to find any news for you</p>
+<?php endif; ?>
