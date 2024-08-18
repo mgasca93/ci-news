68f5a0972b9accaee5f15c575494297a086984d2
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 16:48:42 -0600

    refactor(frontend): change route by url_to method

--- app/Views/news/index.php
+++ app/Views/news/index.php
@@ -1,13 +1,15 @@
 <h2><?= esc( $title ) ?></h2>
-<a href="/news/create">Create new item</a>
+<?php if( session()->get('logged_in') ) : ?>
+<a href="<?= url_to('news_create') ?>">Create new item</a>
 <br><br>
+<?php endif; ?>
 <?php if( $news_list !== [] ) : ?>
     <?php foreach( $news_list as $new_item ) : ?>
         <h3><?= esc( $new_item['title'] ) ?></h3>
         <div class="main">
             <p><?= esc( $new_item['body'] ) ?></p>
         </div>
-        <p><a href="/news/<?= esc($new_item['slug'], 'url') ?>">View article</a></p>
+        <p><a href=" <?= url_to('news_show', esc($new_item['slug'], 'url')) ?>">View article</a></p>
     <?php endforeach; ?>
 <?php else : ?>
     <h3>No news</h3>
