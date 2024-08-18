383143fa423f7f3938acc72ab538bb3a726bea17
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 13:32:56 -0600

    build(backend): add created_at and updated_at at news seeder

--- app/Database/Seeds/News.php
+++ app/Database/Seeds/News.php
@@ -6,25 +6,35 @@ use CodeIgniter\Database\Seeder;
 
 class News extends Seeder
 {
-    protected $notices = [
-        1   => [
-            'title' => 'Elvis sighted',
-            'slug'  => 'elvis-sighted',
-            'body'  => 'Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.',
-        ],
-        2   => [
-            'title' => 'Say it isn\'t so!',
-            'slug'  => 'say-it-isnt-so',
-            'body'  => 'Scientists conclude that some programmers have a sense of humor.'
-        ],
-        3   => [
-            'title' => 'Caffeination, Yes!',
-            'slug'  => 'caffeination-yes',
-            'body'  => 'World\'s largest coffee shop open onsite nested coffee shop for staff only.'
-        ],
-    ];
+    protected $notices = [];
     public function run()
     {
+        helper('date');
+        $this->notices = [
+            1   => [
+                'title'         => 'Elvis sighted',
+                'slug'          => 'elvis-sighted',
+                'body'          => 'Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.',
+                'created_at'    => date('Y-m-d H:i:s', now()),
+                'updated_at'    => date('Y-m-d H:i:s', now()),
+            ],
+            2   => [
+                'title'         => 'Say it isn\'t so!',
+                'slug'          => 'say-it-isnt-so',
+                'body'          => 'Scientists conclude that some programmers have a sense of humor.',
+                'created_at'    => date('Y-m-d H:i:s', now()),
+                'updated_at'    => date('Y-m-d H:i:s', now()),
+            ],
+            3   => [
+                'title'         => 'Caffeination, Yes!',
+                'slug'          => 'caffeination-yes',
+                'body'          => 'World\'s largest coffee shop open onsite nested coffee shop for staff only.',
+                'created_at'    => date('Y-m-d H:i:s', now()),
+                'updated_at'    => date('Y-m-d H:i:s', now()),
+            ],
+        ];
+
+        
         foreach( $this->notices as $notice ) :
             $this->db->table('news')->insert( $notice );
         endforeach;
