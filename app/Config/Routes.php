5e9749debeb7c18a171e35831e09d96c83ea3ee8
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 14:27:46 -0600

    build(backend): add reoutes to login form and authentication

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [\App\Controllers\News::class, 'index']);
 $routes->get('news', [\App\Controllers\News::class, 'index']);
 $routes->get('news/create', [\App\controllers\News::class, 'create']);
 $routes->post('news', [\App\Controllers\News::class, 'store']);
@@ -13,3 +13,6 @@ $routes->get('news/(:segment)',[\App\Controllers\News::class, 'show']);
 
 $routes->get('pages', [\App\Controllers\Pages::class, 'index']);
 $routes->get('(:segment)', [\App\Controllers\Pages::class, 'view']);
+
+$routes->get('auth/login',[\App\Controllers\AuthController::class, 'index']);
+$routes->post('auth/login',[\App\Controllers\AuthController::class, 'authenticate']);
