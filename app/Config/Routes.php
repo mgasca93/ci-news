0f349c7fbc2e0376e1542ed7d0b10d2540d2baaa
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-17 19:03:13 -0600

    build(backend): add routes by news list and new item

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('news', [\App\Controllers\News::class, 'index']);
+$routes->get('news/(:segment)',[\App\Controllers\News::class, 'show']);
+
 $routes->get('pages', [\App\Controllers\Pages::class, 'index']);
 $routes->get('(:segment)', [\App\Controllers\Pages::class, 'view']);
