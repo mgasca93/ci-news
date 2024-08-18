df7386fbc11d844ad56cf8e32a453a15b7ef3ed0
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 14:28:40 -0600

    feat(frontend): add view login form

---
+++ app/Views/auth/login.php
@@ -0,0 +1,24 @@
+<h2><?= esc( $title ) ?></h2>
+
+<?= session()->getFlashdata('error'); ?>
+<?= validation_list_errors(); ?>
+
+<?php if( session()->has('message') ) : ?>
+    <div class="alert alert-success">
+        <p><?= session()->getFlashdata('message') ?></p>
+    </div>
+<?php endif; ?>
+
+<form action="/auth/login" method="POST">
+    <div class="main">
+        <?= csrf_field(); ?>
+        <label for="email">E-mail :</label>
+        <input type="email" id="email" name="email" placeholder="User E-mail" value="<?= set_value('email') ?>" required>
+        <br>
+    
+        <label for="password">Password :</label>
+        <input type="password" id="password" name="password" placeholder="User Password" required>
+    </div>
+    <br>
+    <button type="submit">Login</button>
+</form>
