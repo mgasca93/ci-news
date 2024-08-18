4edefb7e0f1573e1d9eaebb5f7880296e6d66bbd
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 14:28:12 -0600

    build(frontend): add navbar and title property by HTML

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html>
 <head>
-    <title><?php ( strlen( $title ) > 0 ) ? $title : 'CodeIgniter' ?></title>
+    <title><?= ( strlen( $title ) > 0 ) ? $title :'CodeIgniter' ?></title>
 </head>
 <body>
+    <nav>
+        <ul>            
+            <li>
+                <a href="/news">News</a>
+            </li>
+            <li>
+                <a href="/auth/login">Login</a>
+            </li>
+        </ul>
+    </nav>
