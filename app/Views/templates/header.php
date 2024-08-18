bc21cbda569a4afa3bd717854ec849031b87cb5b
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-18 16:48:12 -0600

    feat(frontend): add routes and validation authenticate user

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,10 +7,20 @@
     <nav>
         <ul>            
             <li>
-                <a href="/news">News</a>
+                <a href="<?= url_to('news_list') ?>">News</a>
             </li>
+            <?php if( !session()->get('logged_in') ) : ?>
             <li>
-                <a href="/auth/login">Login</a>
-            </li>
+                <a href="<?= url_to('login_form') ?>">Login</a>
+            </li> 
+            <li>
+                <a href="<?= url_to('register_form') ?>">Register</a>
+            </li> 
+            <?php endif; ?>
+            <?php if( session()->get('logged_in') ) : ?>                       
+            <li>                
+                <span>Welcome <?= session()->get('name') ?></span> <a href="<?= url_to('logout') ?>">Logout</a>
+            </li> 
+            <?php endif; ?>           
         </ul>
     </nav>
