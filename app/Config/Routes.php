55564fadba4c65d3cbff8aeeedbbb88003ba1a70
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-08-17 18:03:53 -0600

    build(backend): add routes by pages controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('pages', [\App\Controllers\Pages::class, 'index']);
+$routes->get('(:segment)', [\App\Controllers\Pages::class, 'view']);
